6b271ba1e73df6cc2a6fce9ad291d316c6b40d6c
Aashish Khadka <aashishkhadka720@gmail.com>
2025-05-15 15:51:07 +0545

    secure production

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure:true,
+    secure: process.env.NODE_ENV === "production",
     sameSite:"none",
     httpOnly: true,
     maxAge: 1000 * 60 * 60 * 24,
